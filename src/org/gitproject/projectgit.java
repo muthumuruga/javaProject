641497ba2e85003f8045b60ba3a23d14ae9d0b7a
muthumuruga <muthumurugan7093@gmail.com>
2023-01-10 19:04:14 +0530

    code added

--- src/org/gitproject/projectgit.java
+++ src/org/gitproject/projectgit.java
@@ -4,6 +4,8 @@ public class projectgit {
 	public static void main(String[] args) {
 		System.out.println("welcome git");
 		System.out.println("muruga");
+	System.out.println("new added");
+	
 	}
 
 }
